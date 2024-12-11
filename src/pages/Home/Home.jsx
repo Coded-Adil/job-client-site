6ef314fb091d758563a5388af9838a11b668e09f
Adil Rahman <itsadil2022@gmail.com>
2024-12-11 20:50:44 +0600

    Woring on registration page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h2>Home Sweet Home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/router/router.jsx
@@ -0,0 +1,25 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/register/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+        ],
+    },
+]);
+
+export default router;
