55c35173461f79fe5ab36d115eb9dbd7c0bd754c
Adil Rahman <itsadil2022@gmail.com>
2024-12-19 23:58:18 +0600

    Added job apply data

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
-    const {id} = useParams();
-    console.log(id);
+    const { id } = useParams();
+    const { user } = useAuth();
+    console.log(id, user);
 
     const submitJobApplication = e => {
         e.preventDefault();
@@ -12,43 +14,41 @@ const JobApply = () => {
         const resume = form.resume.value;
 
         console.log(linkedIn, github, resume);
+
+        // const jobApplication = {
+        //     job_id: id,
+        //     applicant_email: user.email,
+        //     linkedIn,
+        //     github,
+        //     resume,
+        // }
     }
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+        <div className="card w-full">
+            <h1 className="text-5xl text-center font-bold">Apply Here</h1>
+            <form onSubmit={submitJobApplication} className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">LinkedIn URL</span>
+                    </label>
+                    <input type="url" name="linkedIn" placeholder="LinkedIn URL" className="input input-bordered" required />
                 </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form onSubmit={submitJobApplication} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">LinkedIn URL</span>
-                            </label>
-                            <input type="url" name="linkedIn" placeholder="LinkedIn URL" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Github URL</span>
-                            </label>
-                            <input type="url" name="github" placeholder="Github URL" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Resume URL</span>
-                            </label>
-                            <input type="url" name="resume" placeholder="Resume URL" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Apply</button>
-                        </div>
-                    </form>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Github URL</span>
+                    </label>
+                    <input type="url" name="github" placeholder="Github URL" className="input input-bordered" required />
                 </div>
-            </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Resume URL</span>
+                    </label>
+                    <input type="url" name="resume" placeholder="Resume URL" className="input input-bordered" required />
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Apply</button>
+                </div>
+            </form>
         </div>
     );
 };
